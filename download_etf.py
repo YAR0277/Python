e5d71f781320017b8b7c6bbbfeda578794420427
dgore03 <davidgore@protonmail.com>
2026-02-16 15:56:12 -0500

    Initial commit

---
+++ download_etf.py
@@ -0,0 +1,32 @@
+import yfinance as yf
+import pandas as pd
+import time
+from pathlib import Path
+from datetime import datetime
+
+today = datetime.now().strftime("%Y-%m-%d")
+
+# script directory ... Project/Python
+base_path = Path(__file__).resolve().parent
+
+# ETF.txt file path
+tickers_file_path = base_path.parent / "Batch" / "ETF.txt"
+
+# ingest tickers from file
+with open(tickers_file_path, "r") as file:
+    tickers = [line.strip() for line in file if line.strip()]
+
+# download tickers to data folder
+data_folder = base_path.parent.parent / "data" / "finance" / "etf"
+for ticker in tickers:
+    data = yf.download(ticker,period="3mo",interval="1d")
+
+    if hasattr(data.columns, "levels"):
+        data.columns = data.columns.get_level_values(0)
+
+    filename = ticker + "-d.csv"
+    filepath = data_folder / filename
+    data.to_csv(filepath)
+
+    # give server more time to process requests
+    time.sleep(2)
